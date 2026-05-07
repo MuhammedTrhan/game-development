1b1f6bf24b5366424f4df3f87574f199b6e93956
Muhammed Turhan <meheme4434@gmail.com>
2026-05-07 17:14:03 +0200

    Removed assigment 1a's files

--- test_scene.gd
+++ test_scene.gd
@@ -1,24 +1,16 @@
 extends Node2D
 
-@export var message: String = "Hello, World!"
 @export var rotation_speed: float = 1.0
 
 @onready var area_1 = $Area2D
 @onready var area_2 = $Area2D2
 @onready var area_3 = $Area2D3
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print(message)
-	return
-
 
 func _process(delta: float) -> void:
 	area_1.rotation += rotation_speed * delta
 	area_2.rotation -= rotation_speed * delta
 	area_3.rotation += rotation_speed / 2 * delta
-
-	return
 
 
 func _input(event: InputEvent) -> void:
@@ -30,5 +22,3 @@ func _input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("d_pressed"):
 		area_3.modulate = Color.from_hsv(randf(), 0.8, 1.0)
-
-	return
